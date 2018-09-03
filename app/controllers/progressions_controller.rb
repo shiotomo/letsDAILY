3a87074dc4c1ece861a111e1d2a22019767f92fc
shiotomo <t.shiota16@gmail.com>
2018-09-04 00:06:22 +0900

    コメント機能を実装した

--- app/controllers/progressions_controller.rb
+++ app/controllers/progressions_controller.rb
@@ -1,24 +1,38 @@
 class ProgressionsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_task, only: [:create, :edit, :update, :destroy]
+  before_action :set_progression, only: [:edit, :update, :destroy]
 
   def edit
-    @task = Task.find(params[:task_id])
-    @progression = @task.progression.find(params[:id])
   end
 
   def create
-    @task = Task.find(params[:task_id])
-    @task.progression.create(progression_params)
+    @task.progressions.create(progression_params)
     redirect_to task_path(@task)
   end
 
   def update
+    if @progression.update(progression_params)
+      redirect_to task_path(@task)
+    else
+      redirect_to :edit
+    end
   end
 
   def destroy
+    @progression.destroy
+    redirect_to task_path(@task)
   end
 
   private
+  def set_progression
+    @progression = @task.progressions.find(params[:id])
+  end
+
+  def set_task
+    @task = Task.find(params[:task_id])
+  end
+
   def progression_params
     params.require(:progression).permit(:comment, :task_id)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_25_155601) do
+ActiveRecord::Schema.define(version: 2018_08_26_130420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2018_08_25_155601) do
     t.string "title"
     t.text "body"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "progressions", force: :cascade do |t|
+    t.text "comment"
+    t.integer "task_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
